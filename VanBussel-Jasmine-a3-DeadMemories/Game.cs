728e4dc4c287a3a78d95a26cfc12e94059283448
Jamie <Jamie@DESKTOP-UBOP17V>
2024-11-11 21:09:47 -0500

    add circle jump, square enemy, & triangle enemy

--- VanBussel-Jasmine-a3-DeadMemories/Game.cs
+++ VanBussel-Jasmine-a3-DeadMemories/Game.cs
@@ -13,14 +13,18 @@ namespace Game10003
         // Place your variables here:
         Vector2 rectangleLocation = new Vector2(0, 400);
         Vector2 rectangleSize = new Vector2(800, 550);
-        Vector2 circleLocation = new Vector2(100, 400);
+        Vector2 circleLocation = new Vector2(100, 360);
         Vector2 circleLastPosition = new Vector2(200, 200);
+        Vector2 squareLocation = new Vector2(300, 335);
         float circleDirection = 20f;
-        int circleRadius = 40;
+        float jumpHeight = 100f;
+        float circleRadius = 40;
+        float objSpeed = 5;
         Player player = new Player();
 
 
         float recTopangle = 200f;
+        bool isOnGround = false;
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
@@ -44,14 +48,14 @@ namespace Game10003
             Draw.LineSize = 2;
             Draw.Rectangle(-10, 545, 800, 600);
 
-
+            */
             //BODIES
             Draw.FillColor = Color.Gray;
             Draw.LineColor = Color.Black;
             Draw.LineSize = 2;
             float[] xcoords = new float[100];
             float[] ycoords = new float[100];
-            
+            /*
             //Circle Character
             xcoords[0] = 100;
             ycoords[0] = 00;
@@ -62,29 +66,29 @@ namespace Game10003
                 float y = ycoords[i];
                 Draw.Circle(x, y, 45);
             }
-            
-            //Square Character
-            xcoords[0] = 350;
-            ycoords[0] = 480;
-
-            for (int i = 0; i < 1; i++)
-            {
-                float x = xcoords[i];
-                float y = ycoords[i];
-                Draw.Square(x, y, 65);
-            }
-
-            //Triangle Character
-            xcoords[0] = 350;
-            ycoords[0] = 480;
-
-            for (int i = 0; i < 1; i++)
-            {
-                float x = xcoords[i];
-                float y = ycoords[i];
-                Draw.Triangle(600, 350, 650, 450, 550, 450);
-            }
             */
+            //Square Character
+            xcoords[0] = 300;
+            ycoords[0] = 350;
+
+            for (int i = 0; i < 1; i++)
+            {
+                float x = xcoords[i];
+                float y = ycoords[i];
+                Draw.Square(x - 2, y, 55);
+            }
+            
+            //Triangle Character
+            xcoords[0] = 300;
+            ycoords[0] = 430;
+
+            for (int i = 0; i < 1; i++)
+            {
+                float x = xcoords[i];
+                float y = ycoords[i];
+                Draw.Triangle(600, 350, 650, 450, 550, 460);
+            }
+            
 
             {
                 circleLastPosition = circleLocation;
@@ -104,20 +108,22 @@ namespace Game10003
 
         public void RectangleCollision()
         {
-            bool isColliding = circleLocation.Y >= 400f;
-
+            bool isColliding = circleLocation.Y >= 400f - circleRadius;
+            
 
             if (isColliding == true)
             {
-                circleLocation = circleLastPosition;
+                circleLocation.Y = circleLastPosition.Y;
+                isOnGround = true;
             }
+
         }
         public void Jump()
         {
-            if (Input.IsKeyboardKeyDown(KeyboardInput.Space))
+            if (Input.IsKeyboardKeyPressed(KeyboardInput.Space) && isOnGround)
             {
-                circleLocation.Y -= 25f;
-
+                circleLocation.Y -= jumpHeight;
+                isOnGround = false;
             }
         }
     }
